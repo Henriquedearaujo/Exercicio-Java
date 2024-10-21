4acae9e891b47a965a069122ee730f67e92f2c84
Henriquedearaujo <ricke_cris@hotmail.com>
2024-10-21 18:24:08 -0300

    exercicio media das notas

---
+++ TarefaCalculoMedia/src/MediaNotas.java
@@ -0,0 +1,16 @@
+public class MediaNotas {
+    public static void main(String[] args) {
+        double nota1 = 10;
+        double nota2 = 7;
+        double nota3 = 5;
+        double nota4 = 9;
+
+        double notas = (nota1 + nota2 + nota3 + nota4);
+
+        // Calcular a média
+        double media = (notas) / 4;
+
+        // Exibir a média
+        System.out.println("A média das quatro notas é: " + media);
+    }
+}
