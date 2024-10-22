c2bcbc7778534f5ffe7061181b1a5338208cf06b
Henriquedearaujo <ricke_cris@hotmail.com>
2024-10-22 09:08:57 -0300

    Tarefa Wrappers

---
+++ Tarefa_Wrappers/src/TarefaWrappers.java
@@ -0,0 +1,12 @@
+public class TarefaWrappers {
+    public static void main(String[] args) {
+
+        //Tipo Primitivo
+        int num = 1985;
+        
+        //Tipo Wrapper
+        Integer nun1 = num;
+        
+        System.out.println(nun1);
+    }
+}
